2a3388955c97fde1700b87d324b35b22e6f1de9d
Marcus Keßler <damarc@gmx.net>
2014-10-08 14:47:12 +0000

    Fixed Subsets and added some finished Spines(the big spine heads look wonky tho)

--- create_dendrite.lua
+++ create_dendrite.lua
@@ -18,6 +18,11 @@ InitUG(dim, AlgebraType("CPU", 1));
 
 --setSpineNeckRadius(0.2)
 
---void BuildDendrite(number cyt_radius, number er_radius, number dend_length, int pos_app, number app_neck_radius, number app_neck_length, number app_head_radius, number app_head_height, number spine_neck_radius, number spine_neck_length, number spine_head_radius, number spine_head_height, string fileName){
+--void BuildDendrite(number cyt_radius, number er_radius, number dend_length, int pos_app, number app_neck_radius, number app_neck_length, number app_head_radius, number app_head_length, string fileName){
 --BuildDendrite(1.0, 1.0/3.0, 20.0, 5, 0.25, 0.6, 0.1, 0.1, "testCircle.ugx")
-BuildDendrite(2.0, 1.0, 20.0, 2, 0.4, 0.4, 0.1, 0.1, "testCircle.ugx")
+--BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.0, 0.15, 0.6, "normSpineBot.ugx")
+BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.0, 0.15, 0.6, "bigSpineBot.ugx")
+BuildDendrite(2.0, 1.0, 20.0, 10, 0.6, 1.0, 0.25, 0.6, "bigSpineBigAppBot.ugx")
+BuildDendrite(2.0, 1.0, 20.0, 10, 0.6, 1.5, 0.25, 0.6, "bigSpineBigMovedAppBot.ugx")
+BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.5, 0.15, 0.6, "bigSpineMovedAppBot.ugx")
+--BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.5, 0.15, 0.6, "movedAppBot.ugx")
