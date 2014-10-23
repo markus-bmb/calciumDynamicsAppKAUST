a4ce30ed0839cb2726cebc3d3ecbef3b6ab40dab
Marcus Keßler <damarc@gmx.net>
2014-10-23 09:49:49 +0000

    Spine Parameters implemented

--- create_dendrite.lua
+++ create_dendrite.lua
@@ -21,8 +21,8 @@ InitUG(dim, AlgebraType("CPU", 1));
 --void BuildDendrite(number cyt_radius, number er_radius, number dend_length, int pos_app, number app_neck_radius, number app_neck_length, number app_head_radius, number app_head_length, string fileName){
 --BuildDendrite(1.0, 1.0/3.0, 20.0, 5, 0.25, 0.6, 0.1, 0.1, "testCircle.ugx")
 --BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.0, 0.15, 0.6, "normSpineBot.ugx")
-BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.0, 0.15, 0.6, "bigSpineBot.ugx")
-BuildDendrite(2.0, 1.0, 20.0, 10, 0.6, 1.0, 0.25, 0.6, "bigSpineBigAppBot.ugx")
-BuildDendrite(2.0, 1.0, 20.0, 10, 0.6, 1.5, 0.25, 0.6, "bigSpineBigMovedAppBot.ugx")
-BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.5, 0.15, 0.6, "bigSpineMovedAppBot.ugx")
+BuildDendrite(2.0, 1.0, 20.0, 10.0, 0.4, 1.0, 0.15, 0.6, 1.0, 0.5, 0.5, 1.5, "bigNewSpineBot.ugx")
+--BuildDendrite(2.0, 1.0, 20.0, 10, 0.6, 1.0, 0.25, 0.6, "bigSpineBigAppBot.ugx")
+--BuildDendrite(2.0, 1.0, 20.0, 10, 0.6, 1.5, 0.25, 0.6, "bigSpineBigMovedAppBot.ugx")
+--BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.5, 0.15, 0.6, "bigSpineMovedAppBot.ugx")
 --BuildDendrite(2.0, 1.0, 20.0, 10, 0.4, 1.5, 0.15, 0.6, "movedAppBot.ugx")
